f056df9753bd9e8deafb7ad2efb1d1e3188354de
Julio Espinosa <julio.i.espinosa@gmail.com>
2020-04-20 22:04:11 -0500

    added another example
    02 example is added. need to create readme

---
+++ 01-basic/02-multiple-sources-and-headerfile/hello.h
@@ -0,0 +1,14 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+class Hello
+{
+private:
+    /* data */
+public:
+    Hello();
+    ~Hello();
+    static void sayHello();
+};
+
+#endif /* HELLO_H*/
---
+++ 01-basic/02-multiple-sources-and-headerfile/main.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+int main()
+{
+    Hello::sayHello();
+    return 0;
+}
